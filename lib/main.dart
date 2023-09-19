3a268b15e20e3d087f8e4b3fb55163fe31328f83
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2023-09-20 01:09:37 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp/colors.dart';
+import 'package:whatsapp/firebase_options.dart';
+import 'package:whatsapp/screens/mobile_layout_screen.dart';
+import 'package:whatsapp/screens/web_layout_screen.dart';
+import 'package:whatsapp/utils/responsive_layout.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+      
+    );
+  }
+}
